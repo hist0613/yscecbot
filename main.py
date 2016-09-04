9d3b1d38f0721bf910d68cd8116a9ce4878272c0
hist0613 <hist0134@naver.com>
2016-09-04 22:29:29 +0900

    Complete forum alarm function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import pickle
 from urllib.parse import urlparse, parse_qs
 import re
+import time
+import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,13 +15,33 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from slackclient import SlackClient
 
 from settings import *
 
-def main():
+def krtime2datetime(text):
+    # " - 2016년 9월 2일, 금요일, 오후 1:24"
+    # " - 2016년 9월 3일, 토요일, 오후 10:12"
+    # " - 2016년 8월 31일, 수요일, 오전 12:46"
+    # " - 2016년 8월 30일, 화요일, 오후 3:59"
+    # " - 2016년 8월 23일, 화요일, 오전 12:53"
+    # " 씀"
+    text = text.strip()
+    if text == "씀":
+        return datetime.datetime.now()
+    text = text[2:]
+    yyyymmdd, weekday, hhmm = [word.strip() for word in text.split(',')]
+    yyyymmdd = datetime.datetime.strptime(yyyymmdd, "%Y년 %m월 %d일")
+    hhmm = hhmm.replace("오전", "AM")
+    hhmm = hhmm.replace("오후", "PM")
+    hhmm = datetime.datetime.strptime(hhmm, "%p %I:%M")
+    hhmm = datetime.timedelta(hours=hhmm.hour, minutes=hhmm.minute)
+    return yyyymmdd + hhmm
+
+def main(current_time):
     try:
         # 저장된 세션이 존재하는 경우
-        with open("user/{user_id}.session".format(user_id=STUDENT_ID), 'rb') as fp:
+        with open("user/{user_id}/session".format(user_id=STUDENT_ID), 'rb') as fp:
             session = pickle.load(fp)
 
         # 저장된 세션이 존재하지만 로그인에 실패한 경우
@@ -28,7 +50,7 @@ def main():
             raise Exception("NotValidSession")
 
     except:
-        # 저장된 세션이 존재하지 않는 경우 로그인 처리 후 세션 저장
+        # 저장된 세션이 존재하지 않는 경우
         driver = webdriver.Firefox()
         driver.get("http://yscec.yonsei.ac.kr/login/index.php")
         username_input = WebDriverWait(driver, 5).until(
@@ -43,6 +65,8 @@ def main():
         password_input.send_keys(STUDENT_PW)
         password_input.send_keys(Keys.RETURN)
 
+        time.sleep(3)
+
         # http://yscec.yonsei.ac.kr/my/ 로 이동하지 않았다면 로그인이 실패한 상황
         if driver.current_url != "http://yscec.yonsei.ac.kr/my/":
             print("Login Failed!")
@@ -54,10 +78,26 @@ def main():
             session.cookies.set(cookie['name'], cookie['value'])
         driver.close()
 
-        with open("user/{user_id}.session".format(user_id=STUDENT_ID), 'wb') as fp:
+        print("Login Success!")
+
+        # 로그인 처리 후 세션 저장
+        with open("user/{user_id}/session".format(user_id=STUDENT_ID), 'wb') as fp:
             pickle.dump(session, fp)
 
-    # 나의 YSCEC 처리
+    try:
+        with open("user/{user_id}/already_noticed".format(user_id=STUDENT_ID), 'rb') as fp:
+            already_noticed = pickle.load(fp)
+    except FileNotFoundError:
+        already_noticed = set()
+
+    sc = SlackClient(SLACK_TOKEN)
+    if sc.rtm_connect():
+        print("Connection success!")
+    else:
+        print("Connection Failed, invalid token?")
+        return
+
+    # 나의 YSCEC 처리, 과목 리스트 가져오기
     my_page = BeautifulSoup(session.get("http://yscec.yonsei.ac.kr/my/").text, "html.parser")
     for course in my_page.find_all('div', 'course_title'):
         course_link = course.a['href']
@@ -66,6 +106,11 @@ def main():
 
         print('\n' + course_title)
 
+        message = "*" + course_title + "*\n"
+        flag = 0
+
+        # 과목별로 사용하는 게시판(forum)들이 다르기 때문에
+        # 게시판(일반)을 클릭했을 때 나오는 페이지에서 게시판 목록을 가져옴
         forums_link = "http://yscec.yonsei.ac.kr/mod/jinotechboard/index.php?id={course_id}".format(course_id=course_id)
         forums_page = BeautifulSoup(session.get(forums_link).text, "html.parser")
         for forum in forums_page.tbody.find_all('tr'):
@@ -74,6 +119,7 @@ def main():
             forum_link = FORUM_BASE_URL + forum.a['href']
             forum_page = BeautifulSoup(session.get(forum_link).text, "html.parser")
 
+            # 게시판 페이지에서 첫 화면에 보이는 글 목록을 가져오기
             posts = forum_page.find_all('div', 'thread-style')[0]
             posts = [post for post in posts.find_all('li') if post.get('class', [''])[0] != 'isnotice']
             for post in posts:
@@ -81,6 +127,7 @@ def main():
                 post_id = post.find_all('h1', 'thread-post-title')[0].a['onclick']
                 post_id = int(re.search(r'\d+', post_id).group())
 
+                # 글 상세 페이지 가져오기
                 post_page = session.post("http://yscec.yonsei.ac.kr/mod/jinotechboard/content.php",
                             params={'b': forum_id, 'contentId': post_id, 'page': 1, 'perpage': 10, 'boardform': 1}).text
                 post_page = BeautifulSoup(post_page, "html.parser")
@@ -88,13 +135,28 @@ def main():
                 post_title = post_page.find_all('span', 'detail-title')[0].text
                 post_date = post_page.find_all('span', 'detail-date')[0].text
                 post_date = post_date.split(post_page.find_all('span', 'detail-date')[0].a.text)[-1]
+                post_date = krtime2datetime(post_date)
                 post_contents = post_page.find_all('div', 'detail-contents')[0].text
+
+                if (course_id, forum_id, post_id) not in already_noticed:
+                    # 보낼 메시지가 있음을 확인
+                    flag = 1
+                    # 보낸 메시지 목록에 추가
+                    already_noticed.add((course_id, forum_id, post_id))
+                    # 메시지 처리
+                    message += " - %s|%s\n" % (post_date, post_title)
+                    message += "   %s\n" % (' '.join(post_contents[:60].split('\n')))
+
                 # print("제목: {}".format(post_title))
-                # print("날짜: {}".format(post_date))
+                # print("날짜: {}".format(krtime2datetime(post_date)))
                 # print("내용: {}".format(post_contents[:100]))
 
-                # post_date에 "씀"이 들어가있다면, 현재 시간(datetime.datetime.now)를 사용하는게 좋을 듯
-                # post_date(string)을 datetime으로 바꿔야하는데, 앞에 '-'를 지워버리고, 적당히 바꾸면 될 듯
+        if flag:
+            print(message)
+            sc.rtm_send_message(channel="general", message=message)
+
+            with open("user/{user_id}/already_noticed".format(user_id=STUDENT_ID), 'wb') as fp:
+                pickle.dump(already_noticed, fp)
 
         recourses_link = "http://yscec.yonsei.ac.kr/course/resources.php?id={course_id}".format(course_id=course_id)
 
@@ -111,4 +173,4 @@ def main():
     # Grades(점수)에 추가된 내용도 자동으로 알림해주면 좋을 듯
 
 if __name__ == '__main__':
-    main()
+    main(datetime.datetime.now())
